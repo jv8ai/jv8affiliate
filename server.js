731025d667357f30775bd099f9fdf158bfeb8ddc
jv8ai <coreymprice@gmail.com>
2025-08-28 20:13:18 -0600

    test get route

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.post('/webhooks/chargebee', (req, res) => {
   res.send('Webhook received!');
 });
 
+// Add this line for testing
+app.get('/webhooks/chargebee', (req, res) => {
+  res.send('Webhook endpoint is working! (This is a GET test)');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
